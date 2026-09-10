1c251cae05940864f1b2efe425edee059145174f
Thatthada <ponmi19@gmail.com>
2026-09-10 13:26:40 +0700

    Add complete source code and database setup script

---
+++ ProductManagement/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace ProductManagement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSearchKeyword = new System.Windows.Forms.TextBox();
+            this.txtProductId = new System.Windows.Forms.TextBox();
+            this.txtProductName = new System.Windows.Forms.TextBox();
+            this.cmbSearchType = new System.Windows.Forms.ComboBox();
+            this.cmbProductType = new System.Windows.Forms.ComboBox();
+            this.dgvProductMaster = new System.Windows.Forms.DataGridView();
+            this.chkIsActive = new System.Windows.Forms.CheckBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancelProduct = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductMaster)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtSearchKeyword
+            // 
+            this.txtSearchKeyword.Location = new System.Drawing.Point(200, 80);
+            this.txtSearchKeyword.Name = "txtSearchKeyword";
+            this.txtSearchKeyword.Size = new System.Drawing.Size(95, 20);
+            this.txtSearchKeyword.TabIndex = 0;
+            // 
+            // txtProductId
+            // 
+            this.txtProductId.Location = new System.Drawing.Point(484, 80);
+            this.txtProductId.Name = "txtProductId";
+            this.txtProductId.Size = new System.Drawing.Size(114, 20);
+            this.txtProductId.TabIndex = 1;
+            this.txtProductId.Text = "ProductId";
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.Location = new System.Drawing.Point(632, 81);
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.Size = new System.Drawing.Size(100, 20);
+            this.txtProductName.TabIndex = 2;
+            this.txtProductName.Text = "ProductName";
+            // 
+            // cmbSearchType
+            // 
+            this.cmbSearchType.FormattingEnabled = true;
+            this.cmbSearchType.Location = new System.Drawing.Point(329, 80);
+            this.cmbSearchType.Name = "cmbSearchType";
+            this.cmbSearchType.Size = new System.Drawing.Size(121, 21);
+            this.cmbSearchType.TabIndex = 3;
+            this.cmbSearchType.Text = "SearchType";
+            // 
+            // cmbProductType
+            // 
+            this.cmbProductType.FormattingEnabled = true;
+            this.cmbProductType.Location = new System.Drawing.Point(978, 35);
+            this.cmbProductType.Name = "cmbProductType";
+            this.cmbProductType.Size = new System.Drawing.Size(121, 21);
+            this.cmbProductType.TabIndex = 4;
+            this.cmbProductType.Text = "ProductType";
+            // 
+            // dgvProductMaster
+            // 
+            this.dgvProductMaster.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProductMaster.Location = new System.Drawing.Point(200, 116);
+            this.dgvProductMaster.Name = "dgvProductMaster";
+            this.dgvProductMaster.Size = new System.Drawing.Size(899, 316);
+            this.dgvProductMaster.TabIndex = 6;
+            // 
+            // chkIsActive
+            // 
+            this.chkIsActive.AutoSize = true;
+            this.chkIsActive.Location = new System.Drawing.Point(1019, 83);
+            this.chkIsActive.Name = "chkIsActive";
+            this.chkIsActive.Size = new System.Drawing.Size(80, 17);
+            this.chkIsActive.TabIndex = 7;
+            this.chkIsActive.Text = "checkBox1";
+            this.chkIsActive.UseVisualStyleBackColor = true;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(200, 23);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(89, 33);
+            this.btnSearch.TabIndex = 8;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click_1);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(200, 462);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(92, 33);
+            this.btnNew.TabIndex = 9;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click_1);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(342, 462);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(108, 33);
+            this.btnSave.TabIndex = 10;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click_1);
+            // 
+            // btnCancelProduct
+            // 
+            this.btnCancelProduct.Location = new System.Drawing.Point(506, 462);
+            this.btnCancelProduct.Name = "btnCancelProduct";
+            this.btnCancelProduct.Size = new System.Drawing.Size(92, 33);
+            this.btnCancelProduct.TabIndex = 11;
+            this.btnCancelProduct.Text = "Cencle";
+            this.btnCancelProduct.UseVisualStyleBackColor = true;
+            this.btnCancelProduct.Click += new System.EventHandler(this.btnCancelProduct_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1185, 579);
+            this.Controls.Add(this.btnCancelProduct);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.chkIsActive);
+            this.Controls.Add(this.dgvProductMaster);
+            this.Controls.Add(this.cmbProductType);
+            this.Controls.Add(this.cmbSearchType);
+            this.Controls.Add(this.txtProductName);
+            this.Controls.Add(this.txtProductId);
+            this.Controls.Add(this.txtSearchKeyword);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductMaster)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSearchKeyword;
+        private System.Windows.Forms.TextBox txtProductId;
+        private System.Windows.Forms.TextBox txtProductName;
+        private System.Windows.Forms.ComboBox cmbSearchType;
+        private System.Windows.Forms.ComboBox cmbProductType;
+        private System.Windows.Forms.DataGridView dgvProductMaster;
+        private System.Windows.Forms.CheckBox chkIsActive;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancelProduct;
+    }
+}
+
---
+++ ProductManagement/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace ProductManagement
+{
+    public partial class Form1 : Form
+    {
+        private readonly string connectionString = "Server=localhost;Database=Product;Trusted_Connection=True;TrustServerCertificate=True;";
+        private string currentUser = "ADMIN";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadProductTypes();
+            SearchProducts();
+        }
+
+        private void LoadProductTypes()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    string sql = "SELECT PRODUCTTYPEID, PRODUCTTYPENAME FROM PRODUCTTYPE";
+                    SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    cmbProductType.DataSource = dt;
+                    cmbProductType.DisplayMember = "PRODUCTTYPENAME";
+                    cmbProductType.ValueMember = "PRODUCTTYPEID";
+
+                    DataTable dtSearch = dt.Copy();
+                    DataRow row = dtSearch.NewRow();
+                    row["PRODUCTTYPEID"] = "";
+                    row["PRODUCTTYPENAME"] = "-- ทั้งหมด --";
+                    dtSearch.Rows.InsertAt(row, 0);
+
+                    cmbSearchType.DataSource = dtSearch;
+                    cmbSearchType.DisplayMember = "PRODUCTTYPENAME";
+                    cmbSearchType.ValueMember = "PRODUCTTYPEID";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("เกิดข้อผิดพลาดในการโหลดประเภทสินค้า: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void SearchProducts()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    string sql = @"SELECT p.PRODUCTID, p.PRODUCTNAME, t.PRODUCTTYPENAME, 
+                                          CASE WHEN p.PRODUCTSTATUS = 0 THEN N'ใช้งาน' ELSE N'ยกเลิก' END AS STATUS_NAME,
+                                          p.PRODUCTSTATUS, p.PRODUCTTYPEID, p.CREATEUSER, p.CREATEDATE
+                                   FROM PRODUCTMASTER p
+                                   INNER JOIN PRODUCTTYPE t ON p.PRODUCTTYPEID = t.PRODUCTTYPEID
+                                   WHERE (p.PRODUCTID LIKE @Keyword OR p.PRODUCTNAME LIKE @Keyword)";
+
+                    if (cmbSearchType.SelectedValue != null && !string.IsNullOrEmpty(cmbSearchType.SelectedValue.ToString()))
+                    {
+                        sql += " AND p.PRODUCTTYPEID = @TypeId";
+                    }
+
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@Keyword", "%" + txtSearchKeyword.Text.Trim() + "%");
+                    if (cmbSearchType.SelectedValue != null && !string.IsNullOrEmpty(cmbSearchType.SelectedValue.ToString()))
+                    {
+                        cmd.Parameters.AddWithValue("@TypeId", cmbSearchType.SelectedValue.ToString());
+                    }
+
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    dgvProductMaster.DataSource = dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("เกิดข้อผิดพลาดในการค้นหาข้อมูล: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            SearchProducts();
+        }
+
+        private void btnSearch_Click_1(object sender, EventArgs e)
+        {
+            SearchProducts();
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            ClearForm();
+        }
+
+        private void btnNew_Click_1(object sender, EventArgs e)
+        {
+            ClearForm();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtProductId.Text) || string.IsNullOrWhiteSpace(txtProductName.Text))
+            {
+                MessageBox.Show("กรุณากรอกรหัสและชื่อสินค้าให้ครบถ้วน", "แจ้งเตือน", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string checkSql = "SELECT COUNT(1) FROM PRODUCTMASTER WHERE PRODUCTID = @ProductId";
+                    SqlCommand checkCmd = new SqlCommand(checkSql, conn);
+                    checkCmd.Parameters.AddWithValue("@ProductId", txtProductId.Text.Trim());
+                    int count = Convert.ToInt32(checkCmd.ExecuteScalar());
+
+                    int status = chkIsActive.Checked ? 0 : 9;
+
+                    if (count == 0)
+                    {
+ 
+                        string insertSql = @"INSERT INTO PRODUCTMASTER 
+                                            (PRODUCTID, PRODUCTNAME, PRODUCTTYPEID, PRODUCTSTATUS, CREATEUSER, CREATEDATE)
+                                             VALUES (@ProductId, @ProductName, @TypeId, @Status, @User, GETDATE())";
+                        SqlCommand insCmd = new SqlCommand(insertSql, conn);
+                        insCmd.Parameters.AddWithValue("@ProductId", txtProductId.Text.Trim());
+                        insCmd.Parameters.AddWithValue("@ProductName", txtProductName.Text.Trim());
+                        insCmd.Parameters.AddWithValue("@TypeId", cmbProductType.SelectedValue.ToString());
+                        insCmd.Parameters.AddWithValue("@Status", status);
+                        insCmd.Parameters.AddWithValue("@User", currentUser);
+                        insCmd.ExecuteNonQuery();
+
+                        MessageBox.Show("เพิ่มข้อมูลสำเร็จ", "ผลการทำงาน", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+               
+                        string updateSql = @"UPDATE PRODUCTMASTER 
+                                             SET PRODUCTNAME = @ProductName, 
+                                                 PRODUCTTYPEID = @TypeId, 
+                                                 PRODUCTSTATUS = @Status, 
+                                                 UPDATEUSER = @User, 
+                                                 UPDATEDATE = GETDATE()
+                                             WHERE PRODUCTID = @ProductId";
+                        SqlCommand updCmd = new SqlCommand(updateSql, conn);
+                        updCmd.Parameters.AddWithValue("@ProductId", txtProductId.Text.Trim());
+                        updCmd.Parameters.AddWithValue("@ProductName", txtProductName.Text.Trim());
+                        updCmd.Parameters.AddWithValue("@TypeId", cmbProductType.SelectedValue.ToString());
+                        updCmd.Parameters.AddWithValue("@Status", status);
+                        updCmd.Parameters.AddWithValue("@User", currentUser);
+                        updCmd.ExecuteNonQuery();
+
+                        MessageBox.Show("แก้ไขข้อมูลสำเร็จ", "ผลการทำงาน", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+
+                    SearchProducts();
+                    ClearForm();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("เกิดข้อผิดพลาดในการบันทึกข้อมูล: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSave_Click_1(object sender, EventArgs e)
+        {
+            btnSave_Click(sender, e);
+        }
+
+        private void btnCancelProduct_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtProductId.Text))
+            {
+                MessageBox.Show("กรุณาเลือกรหัสสินค้าที่ต้องการยกเลิก", "แจ้งเตือน", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (MessageBox.Show("คุณต้องการยกเลิกสินค้ารหัสนี้ใช่หรือไม่?", "ยืนยัน", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                try
+                {
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        conn.Open();
+                        string sql = @"UPDATE PRODUCTMASTER 
+                                       SET PRODUCTSTATUS = 9, 
+                                           UPDATEUSER = @User, 
+                                           UPDATEDATE = GETDATE()
+                                       WHERE PRODUCTID = @ProductId";
+                        SqlCommand cmd = new SqlCommand(sql, conn);
+                        cmd.Parameters.AddWithValue("@ProductId", txtProductId.Text.Trim());
+                        cmd.Parameters.AddWithValue("@User", currentUser);
+                        cmd.ExecuteNonQuery();
+
+                        MessageBox.Show("ยกเลิกรายการสินค้าเรียบร้อยแล้ว", "ผลการทำงาน", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        SearchProducts();
+                        ClearForm();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("เกิดข้อผิดพลาดในการยกเลิกสินค้า: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnCancelProduct_Click_1(object sender, EventArgs e)
+        {
+            btnCancelProduct_Click(sender, e);
+        }
+
+      
+        private void dgvProductMaster_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dgvProductMaster.Rows[e.RowIndex];
+                txtProductId.Text = row.Cells["PRODUCTID"].Value.ToString();
+                txtProductName.Text = row.Cells["PRODUCTNAME"].Value.ToString();
+                cmbProductType.SelectedValue = row.Cells["PRODUCTTYPEID"].Value.ToString();
+
+                int status = Convert.ToInt32(row.Cells["PRODUCTSTATUS"].Value);
+                chkIsActive.Checked = (status == 0);
+
+                txtProductId.ReadOnly = true; 
+            }
+        }
+        private void ClearForm()
+        {
+            txtProductId.Text = "";
+            txtProductName.Text = "";
+            txtProductId.ReadOnly = false;
+            chkIsActive.Checked = true;
+            if (cmbProductType.Items.Count > 0) cmbProductType.SelectedIndex = 0;
+        }
+    }
+}
